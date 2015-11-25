e29fd012dd6ea8e00dbbffa1b79013f7275fd4e6
alexknipfer <cr33pyxjtag@yahoo.com>
2015-11-25 14:45:15 -0600

    added project 4

---
+++ Project4/Project4/Form1.Designer.vb
@@ -0,0 +1,291 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.createFile = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.sizeChoice = New System.Windows.Forms.ComboBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.imageRB = New System.Windows.Forms.RadioButton()
+        Me.grayRB = New System.Windows.Forms.RadioButton()
+        Me.oliveRB = New System.Windows.Forms.RadioButton()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'createFile
+        '
+        Me.createFile.Location = New System.Drawing.Point(326, 232)
+        Me.createFile.Name = "createFile"
+        Me.createFile.Size = New System.Drawing.Size(75, 23)
+        Me.createFile.TabIndex = 0
+        Me.createFile.Text = "Create File"
+        Me.createFile.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(407, 232)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Exit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.Panel1.BackColor = System.Drawing.Color.White
+        Me.Panel1.Controls.Add(Me.GroupBox2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.sizeChoice)
+        Me.Panel1.Controls.Add(Me.GroupBox1)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.createFile)
+        Me.Panel1.Location = New System.Drawing.Point(-1, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(497, 268)
+        Me.Panel1.TabIndex = 3
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.TextBox4)
+        Me.GroupBox2.Controls.Add(Me.TextBox2)
+        Me.GroupBox2.Controls.Add(Me.TextBox3)
+        Me.GroupBox2.Controls.Add(Me.TextBox1)
+        Me.GroupBox2.Location = New System.Drawing.Point(7, 154)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(482, 58)
+        Me.GroupBox2.TabIndex = 6
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Set Up Pairs"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(375, 25)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(41, 13)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Yellow:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(261, 25)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Green:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(149, 25)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(30, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Red:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(31, 25)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(31, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Blue:"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(421, 22)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(27, 20)
+        Me.TextBox4.TabIndex = 2
+        Me.TextBox4.Text = "1"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(187, 22)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(27, 20)
+        Me.TextBox2.TabIndex = 0
+        Me.TextBox2.Text = "2"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(306, 22)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(27, 20)
+        Me.TextBox3.TabIndex = 1
+        Me.TextBox3.Text = "2"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(70, 22)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(27, 20)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 232)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(117, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Select what size board:"
+        '
+        'sizeChoice
+        '
+        Me.sizeChoice.FormattingEnabled = True
+        Me.sizeChoice.Location = New System.Drawing.Point(154, 228)
+        Me.sizeChoice.Name = "sizeChoice"
+        Me.sizeChoice.Size = New System.Drawing.Size(121, 21)
+        Me.sizeChoice.TabIndex = 4
+        Me.sizeChoice.Text = "4"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.PictureBox2)
+        Me.GroupBox1.Controls.Add(Me.imageRB)
+        Me.GroupBox1.Controls.Add(Me.grayRB)
+        Me.GroupBox1.Controls.Add(Me.oliveRB)
+        Me.GroupBox1.Location = New System.Drawing.Point(7, 90)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(482, 57)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Card Back"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(417, 10)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(59, 41)
+        Me.PictureBox2.TabIndex = 3
+        Me.PictureBox2.TabStop = False
+        '
+        'imageRB
+        '
+        Me.imageRB.AutoSize = True
+        Me.imageRB.Location = New System.Drawing.Point(340, 24)
+        Me.imageRB.Name = "imageRB"
+        Me.imageRB.Size = New System.Drawing.Size(54, 17)
+        Me.imageRB.TabIndex = 2
+        Me.imageRB.Text = "Image"
+        Me.imageRB.UseVisualStyleBackColor = True
+        '
+        'grayRB
+        '
+        Me.grayRB.AutoSize = True
+        Me.grayRB.Location = New System.Drawing.Point(180, 24)
+        Me.grayRB.Name = "grayRB"
+        Me.grayRB.Size = New System.Drawing.Size(47, 17)
+        Me.grayRB.TabIndex = 1
+        Me.grayRB.Text = "Gray"
+        Me.grayRB.UseVisualStyleBackColor = True
+        '
+        'oliveRB
+        '
+        Me.oliveRB.AutoSize = True
+        Me.oliveRB.Checked = True
+        Me.oliveRB.Location = New System.Drawing.Point(20, 24)
+        Me.oliveRB.Name = "oliveRB"
+        Me.oliveRB.Size = New System.Drawing.Size(49, 17)
+        Me.oliveRB.TabIndex = 0
+        Me.oliveRB.TabStop = True
+        Me.oliveRB.Text = "Olive"
+        Me.oliveRB.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources.header1
+        Me.PictureBox1.Location = New System.Drawing.Point(0, -31)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(497, 114)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(493, 267)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents createFile As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents imageRB As RadioButton
+    Friend WithEvents grayRB As RadioButton
+    Friend WithEvents oliveRB As RadioButton
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents sizeChoice As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Project4/Project4/Form1.vb
@@ -0,0 +1,58 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Private radioButtonSelected As String
+    Private sizeChosen As Integer
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+        sizeChoice.Items.Add("4")   'add 4 to dropdown choice
+        sizeChoice.Items.Add("5")   'add 5 to dropdown choice
+        sizeChoice.Items.Add("6")   'add 6 to dropdown choice
+        sizeChoice.Items.Add("7")   'add 7 to dropdown choice
+        sizeChoice.Items.Add("8")   'add 8 to dropdown choice
+        sizeChoice.Items.Add("9")   'add 9 to dropdown choice
+        sizeChoice.Items.Add("10")  'add 10 to dropdown choice
+
+        radioButtonSelected = "Olive"   'set default card back color to olive
+        sizeChoice.SelectedItem = "4"   'set default game size to 4
+
+    End Sub
+
+
+    Private Sub createFile_Click(sender As Object, e As EventArgs) Handles createFile.Click
+
+        Dim dialog As SaveFileDialog
+        dialog = New SaveFileDialog     'create new save file dialog
+        dialog.ShowDialog()             'show the user the dialog
+
+        'create the file writer
+        Dim fileWriter As StreamWriter
+        fileWriter = New StreamWriter(dialog.FileName)
+
+        'see which radio button was selected
+        If (oliveRB.Checked) Then
+            radioButtonSelected = "Olive"   'store string olive if olive was selected
+
+        ElseIf (grayRB.Checked) Then
+            radioButtonSelected = "Gray"    'store string gray if gray was selected
+        End If
+
+        sizeChosen = CInt(sizeChoice.SelectedItem.ToString) 'get the size selected from dropdown
+
+        fileWriter.WriteLine(radioButtonSelected)   'print which card back color to file
+        fileWriter.WriteLine(sizeChosen)            'print size board they want to file
+        fileWriter.Close()
+
+    End Sub
+
+    Private Sub imageRB_CheckedChanged(sender As Object, e As EventArgs) Handles imageRB.CheckedChanged
+
+    End Sub
+End Class
